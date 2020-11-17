3a9eda28aa57ce2e1e6ba787ae13734541a91e32
Maryna Korzhyk <cavilosa33@gmail.com>
2020-11-17 14:05:20 -0700

    nav in process

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-/**
- **
+/*
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
@@ -13,10 +13,15 @@
  *
 */
 
-/**
+/*
  * Define Global Variables
  *
 */
+
+// Variable to append li items
+const navList = document.querySelector("ul");
+
+const sections = document.querySelectorAll("section");
 
 
 /**
@@ -32,15 +37,26 @@
  * Begin Main Functions
  *
 */
+function scrollToElement (element) {
+    element.scrollIntoView();
+}
+
 
 // build the nav
+function listItems () {
+    for (const section of sections) {
+        const li = section.getAttribute("data-nav");
+        navList.innerHTML += `<li class="menu__link">${li}</li>`;
+    }
+    return navList;
+}
 
+listItems();
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
